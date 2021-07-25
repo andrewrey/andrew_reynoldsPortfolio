7dfa7f67503a730b24d8953054741f5565e3e94b
Andrew Reynolds <andrew_reynolds@mac.com>
2021-07-25 09:20:47 -0700

    initial file structure of src folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+import Header from "./Components/header/Header";
 
 function App() {
-  return <div className="App"></div>;
+  <>
+    <Header />
+    <Main />
+    <Footer />
+  </>;
 }
 
 export default App;
