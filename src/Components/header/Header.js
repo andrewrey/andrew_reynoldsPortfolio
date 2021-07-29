8617ba0bc1949b4dd750e9278d64cedb33ad0a84
Andrew Reynolds <andrew_reynolds@mac.com>
2021-07-28 20:42:53 -0700

    moved useEffect and handlers to parent component of the Mobile nav which is Header

--- src/Components/header/Header.js
+++ src/Components/header/Header.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import styles from "./Header.module.scss";
 import MobileNav from "./MobileNav";
 import Button from "../../UI/Button";
@@ -5,9 +6,34 @@ import Wrap from "../../UI/Wrap";
 import Nav from "./Nav";
 
 const Header = () => {
+  const [viewModal, setViewModal] = useState(false);
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+  const toggleViewHandler = () => {
+    setViewModal((prevState) => !prevState);
+  };
+
+  useEffect(() => {
+    console.log("effect");
+    const windowResizeHandler = () => {
+      if (window.innerWidth > 768) {
+        setViewModal(false);
+      }
+      setWindowSize(window.innerWidth);
+    };
+    window.addEventListener("resize", windowResizeHandler);
+    return () => {
+      window.removeEventListener("resize", windowResizeHandler);
+    };
+  }, []);
   return (
     <header className={styles.header}>
-      <MobileNav />
+      {windowSize < 768 && (
+        <MobileNav
+          onToggle={toggleViewHandler}
+          window={windowSize}
+          viewModal={viewModal}
+        />
+      )}
       <Wrap name={styles["header__wrap"]}>
         <h1>
           Andrew Reynolds <span>Front End Developer</span>
--- src/Components/header/MobileNav.js
+++ src/Components/header/MobileNav.js
@@ -1,41 +1,25 @@
-import { useState, useEffect } from "react";
 import Logo from "../../UI/Logo";
 import Modal from "../../UI/Modal";
 import Nav from "./Nav";
 import Hamburger from "../../UI/Hamburger";
 import Wrap from "../../UI/Wrap";
 import styles from "./MobileNav.module.scss";
+import { useEffect } from "react";
 
-const MobileNav = () => {
-  const [viewModal, setViewModal] = useState(false);
-  const [windowSize, setWindowSize] = useState(window.innerWidth);
-  const toggleViewHandler = () => {
-    setViewModal((prevState) => !prevState);
-  };
-
-  useEffect(() => {
-    const windowResizeHandler = () => {
-      setWindowSize(window.innerWidth);
-    };
-    window.addEventListener("resize", windowResizeHandler);
-    return () => window.removeEventListener("resize", windowResizeHandler);
-  }, []);
-
+const MobileNav = ({ onToggle, viewModal }) => {
   return (
     <>
-      {windowSize < 768 && (
-        <div className={styles.header__mobile_nav}>
-          {viewModal && (
-            <Modal onClose={toggleViewHandler}>
-              <Nav />
-            </Modal>
-          )}
-          <Wrap name={styles.mobile__wrap}>
-            <Logo />
-            <Hamburger onToggle={toggleViewHandler} />
-          </Wrap>
-        </div>
-      )}
+      <div className={styles.header__mobile_nav}>
+        {viewModal && (
+          <Modal onClose={onToggle}>
+            <Nav />
+          </Modal>
+        )}
+        <Wrap name={styles.mobile__wrap}>
+          <Logo />
+          <Hamburger onToggle={onToggle} />
+        </Wrap>
+      </div>
     </>
   );
 };
