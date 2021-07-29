0575b9b182d51b64966e2ec4ecaf275bbc5fa8e0
Andrew Reynolds <andrew_reynolds@mac.com>
2021-07-28 19:46:14 -0700

    added cleanup to useEffect hook

--- src/Components/header/MobileNav.js
+++ src/Components/header/MobileNav.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Logo from "../../UI/Logo";
 import Modal from "../../UI/Modal";
 import Nav from "./Nav";
@@ -8,21 +8,35 @@ import styles from "./MobileNav.module.scss";
 
 const MobileNav = () => {
   const [viewModal, setViewModal] = useState(false);
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
   const toggleViewHandler = () => {
     setViewModal((prevState) => !prevState);
   };
+
+  useEffect(() => {
+    const windowResizeHandler = () => {
+      setWindowSize(window.innerWidth);
+    };
+    window.addEventListener("resize", windowResizeHandler);
+    return () => window.removeEventListener("resize", windowResizeHandler);
+  }, []);
+
   return (
-    <div className={styles.header__mobile_nav}>
-      {viewModal && (
-        <Modal onClose={toggleViewHandler}>
-          <Nav />
-        </Modal>
+    <>
+      {windowSize < 768 && (
+        <div className={styles.header__mobile_nav}>
+          {viewModal && (
+            <Modal onClose={toggleViewHandler}>
+              <Nav />
+            </Modal>
+          )}
+          <Wrap name={styles.mobile__wrap}>
+            <Logo />
+            <Hamburger onToggle={toggleViewHandler} />
+          </Wrap>
+        </div>
       )}
-      <Wrap name={styles.mobile__wrap}>
-        <Logo />
-        <Hamburger onToggle={toggleViewHandler} />
-      </Wrap>
-    </div>
+    </>
   );
 };
 
