353dd53684126e5d297ee8af1bed174d8ff4a644
Andrew Reynolds <andrew_reynolds@mac.com>
2021-07-30 08:46:50 -0700

    inital jsx of background section

--- src/Components/main/mainComponents/Background.js
+++ src/Components/main/mainComponents/Background.js
@@ -3,12 +3,23 @@ import styles from "./Background.module.scss";
 const Background = () => {
   return (
     <section>
-      <h2>Journey to being a developer</h2>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div className={styles.background__wrap}>
+        <div>
+          <h2>
+            Journey to becoming a <span>developer</span>
+            <br />
+            The Background Story
+          </h2>
+        </div>
+        <div>
+          <div></div>
+          <div>
+            <div></div>
+            <div></div>
+          </div>
+          <div></div>
+        </div>
+      </div>
     </section>
   );
 };
