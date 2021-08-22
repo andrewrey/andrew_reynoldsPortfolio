c4611549bce00b631ef6a2224c329978c44d1b49
Andrew Reynolds <andrew_reynolds@mac.com>
2021-08-21 21:40:41 -0700

    basic projects layout

--- src/Components/main/mainComponents/Projects.js
+++ src/Components/main/mainComponents/Projects.js
@@ -1,7 +1,11 @@
 import styles from "./Projects.module.scss";
 
 const Projects = () => {
-  return <section></section>;
+  return (
+    <section className={styles.project}>
+      <div className={styles.project__wrap}></div>
+    </section>
+  );
 };
 
 export default Projects;
